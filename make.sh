758da8a265b077120b89fcb35074bd0e21b09676
dragonfi <david.gabor.bodor@gmail.com>
2014-03-29 12:13:04 +0100

    Improve make script.

--- make.sh
+++ make.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-pandoc resume.markdown -V fontsize=12pt -V papersize:a4paper -V geometry:margin=3cm -o resume.pdf
+for i in $(find . -iname '*resume.markdown')
+do
+	o=${i/.markdown/.pdf}
+	[ $o -ot $i ] && echo "making $o" &&
+	pandoc $i -V fontsize=12pt -V papersize:a4paper -V geometry:margin=3cm -o $o
+done
+
